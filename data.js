61930fe7fd95005627f08d75edc354e24bfb1cc3
HamidNE <hamidne@mail.ru>
2019-12-08 13:23:18 +0330

    add links for post

--- data.js
+++ data.js
@@ -1,15 +1,22 @@
 export default {
 	title_en: "CreativeMarket - Sunflora - Unique Ligature Font 3737195",
 	title_fa: "فونت بسیار زیبای سانفلورا",
-	description: "در این پست فونت بسیار زیبای سانفلورا برای دانلود در ایران جی اف ایکس قرار گرفته است. شما از این فونت می توانید در طراحی هایی صفحه آرایی بروشور و کاتالوگ استفاده کنید. ",
+	description:
+		"در این پست فونت بسیار زیبای سانفلورا برای دانلود در ایران جی اف ایکس قرار گرفته است. شما از این فونت می توانید در طراحی هایی صفحه آرایی بروشور و کاتالوگ استفاده کنید. ",
 	file_type: "OTF",
-	source: "https://creativemarket.com/NewTropical/3737195-Sunflora-Unique-Ligature-Font",
+	source:
+		"https://creativemarket.com/NewTropical/3737195-Sunflora-Unique-Ligature-Font",
 	price: "$14",
 	schedule: {
 		day: "12",
 		month: "10",
 		year: "1398",
 		minus: "0",
-		hour: "22",
-	}
-}
+		hour: "22"
+	},
+	premium: true,
+	links: [
+		"/domains/pz10448.parspack.net/public_html/premium/photo/20-Photography-Digital-Backgrounds.irangfx.com.rar",
+		"/domains/pz10448.parspack.net/public_html/date-post/1398/1398-5-8/Voucher-Gift-Cards-Template_irangfx.com.rar"
+	]
+};
