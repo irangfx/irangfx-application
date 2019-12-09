7566c2b32eb961c7f7ed5962e13c4f852a23cfe9
HamidNE <hamidne@mail.ru>
2019-12-09 11:29:54 +0330

    edit for type inputs

--- main.js
+++ main.js
@@ -2,28 +2,17 @@ import puppeteer from "puppeteer";
 import data from "./data";
 require("dotenv").config();
 
-// function optimizeSpeed(params) {
-// 	await page.setRequestInterception(true);
-// 	page.on('request', request => {
-// 		if (['font'].includes(request.resourceType())) {
-// 			request.abort();
-// 		} else {
-// 			request.continue();
-// 		}
-// 	});
-// }
+const delay = 10;
 
 (async () => {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
-
 	await page.setViewport({ width: 800, height: 1000 });
 
+	// await optimizeSpeed(page);
 	await prepareLogin(page);
 
-	await page.goto("https://irangfx.com/wp-admin/post-new.php", {
-		timeout: 0
-	});
+	await page.goto("https://irangfx.com/wp-admin/post-new.php", { timeout: 0 });
 
 	await preparePostTitle(page);
 	await preparePostLink(page);
@@ -37,31 +26,47 @@ require("dotenv").config();
 	// await browser.close();
 })();
 
+async function optimizeSpeed(page) {
+	await page.setRequestInterception(true);
+	page.on("request", request => {
+		if (["font", "stylesheet"].includes(request.resourceType())) {
+			request.abort();
+		} else {
+			request.continue();
+		}
+	});
+}
+
 async function prepareLogin(page) {
-	await page.goto("https://irangfx.com/login-users/?loggedout=true");
+	await page.goto("https://irangfx.com/login-users/");
+
 	while ((await page.url()) !== "https://irangfx.com/wp-admin/") {
-		await page.type("#user_login", process.env.USERNAME);
-		await page.type("#user_pass", process.env.PASSWORD);
+		await typeValue(page, "#user_login", process.env.USERNAME);
+		await typeValue(page, "#user_pass", process.env.PASSWORD);
 		await page.click("#wp-submit");
 	}
 }
 
 async function preparePostTags(page) {
-	await page.type("#new-tag-post_tag", data.tags.join(","));
+	await typeValue(page, "#new-tag-post_tag", data.tags.join(","));
 	await page.click("input.button.tagadd");
 }
 
 async function preparePostSoftware(page) {
-	await page.click("#acf-field_5c04d5eb6d4fd", { clickCount: 3 });
-	await page.type("#acf-field_5c04d5eb6d4fd", `Download ${data.title_en}`);
-	
+	await typeValue(
+		page,
+		"#acf-field_5c04d5eb6d4fd",
+		`Download ${data.title_en}`
+	);
+
 	if (data.formats.includes("PSD")) {
 		await page.click(".acf-field-5d18689195c43 ul>li:nth-child(1)");
 	}
 	if (data.formats.includes("AI")) {
 		await page.click(".acf-field-5d18689195c43 ul>li:nth-child(2)");
 	}
-	if (data.price) await page.type("#acf-field_5c04d83864846", data.price);
+	if (data.price)
+		await page.type("#acf-field_5c04d83864846", data.price, { delay });
 	for (const format of data.formats)
 		await page.click(`.acf-field-5c04d88e64847 input[value=${format}]`);
 	if (data.formats.includes("PDF")) {
@@ -83,13 +88,24 @@ async function preparePostPremium(page) {
 async function preparePostSlug(page) {
 	await page.waitForSelector(".edit-slug.button");
 	await page.click(".edit-slug.button");
-	await page.click("#new-post-slug", { clickCount: 3 });
-	await page.type("#new-post-slug", data.title_en);
+
+	await typeValue(page, "#new-post-slug", data.title_en);
+
+	await page.waitForSelector("#edit-slug-buttons button.save");
 	await page.click("#edit-slug-buttons button.save");
 }
 
+async function typeValue(page, selector, value) {
+	const searchInput = await page.$(selector);
+	await searchInput.click({ clickCount: 3 });
+	await searchInput.press("Backspace");
+	await searchInput.type(value);
+}
+
 async function preparePostTitle(page) {
-	await page.type("#title-prompt-text", `${data.title_fa} - ${data.title_en}`);
+	await page.type("#title-prompt-text", `${data.title_fa} - ${data.title_en}`, {
+		delay
+	});
 }
 
 async function preparePostLink(page) {
@@ -101,14 +117,15 @@ async function preparePostLink(page) {
 		if (index > 0) await page.click(`${divId} .acf-actions a.acf-button`);
 		await page.click(`${divId} tbody tr:nth-child(${index + 1}) a.button`);
 		await page.waitForSelector("#link-options");
-		await page.type(
+		await typeValue(
+			page,
 			"#wp-link-url",
 			link.replace(
 				"/domains/pz10448.parspack.net/public_html",
 				"https://dl.irangfx.com"
 			)
 		);
-		await page.type("#wp-link-text", "دانلود این موکاپ");
+		await typeValue(page, "#wp-link-text", "دانلود این موکاپ");
 		await page.click("#wp-link-target");
 		await page.click("#wp-link-update");
 	}
@@ -117,9 +134,9 @@ async function preparePostLink(page) {
 async function preparePostSchedule(page) {
 	await page.click("a[href='#edit_timestamp'].edit-timestamp");
 	await page.select("#Jmm", data.schedule.month);
-	await page.type("#Jjj", data.schedule.month);
-	await page.type("#Jaa", data.schedule.month);
-	await page.type("#Jmn", data.schedule.minus);
-	await page.type("#Jhh", data.schedule.hour);
+	await typeValue(page, "#Jjj", data.schedule.day);
+	await typeValue(page, "#Jaa", data.schedule.year);
+	await typeValue(page, "#Jmn", data.schedule.minus);
+	await typeValue(page, "#Jhh", data.schedule.hour);
 	await page.click("a[href='#edit_timestamp'].save-timestamp");
 }
